3782220e91c77f9d03ce161f76f20b7ef7578db3
MarkAnthony9014 <markanthony9014@gmail.com>
2022-08-03 00:04:22 -0500

    added submit eventHandler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 //Establishing the button element object representation by using query selector to find the id in the button element.
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
 
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-}
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = "This is a new task."; 
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
 
-buttonEl.addEventListener("click", createTaskHandler);
+  formEl.addEventListener("submit", createTaskHandler);
