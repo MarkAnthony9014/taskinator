2a45cf84248f749872ca8ea22180058beacd11da
MarkAnthony9014 <markanthony9014@gmail.com>
2022-08-02 22:11:41 -0500

    add ability to add task

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+//Establishing the button element object representation by using query selector to find the id in the button element.
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+/*buttonEl.addEventListener("click", function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+});*/
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+buttonEl.addEventListener("click", createTaskHandler);
